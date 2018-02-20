5f018b486f5ec63007f39a96d8140649f39dfc64
john <john@lashl.ee>
2018-02-20 00:16:26 -0800

    Changing my parsing functions to align with MTG Goldfish

--- R/utility-fns.R
+++ R/utility-fns.R
@@ -1,10 +1,19 @@
-goldfish_url <- function(card_name, set) {
-  paste0("https://www.mtggoldfish.com/price/",
-         str_replace_all(set," ","+"),
-         "/",
-         str_replace_all(card_name," ","+"))
-}
 contains_price_data <- function(script,card_name) {
   all(str_detect(script, paste0("Date,",card_name)),
       str_detect(script, "20[0-9]{2}-[0-9]{2}-[0-9]{2}, \\d+\\.*\\d*"))
+}
+
+sanitize_name <- function(x) {
+  str_replace_all(x, " ", "+") %>%
+    str_replace_all("'","") %>%
+    str_replace_all(",","") %>%
+    str_replace_all(":","") %>%
+    str_replace_all("\\.","")
+}
+
+goldfish_url <- function(card_name, set) {
+  paste0("https://www.mtggoldfish.com/price/",
+         sanitize_name(set),
+         "/",
+         sanitize_name(card_name))
 }
